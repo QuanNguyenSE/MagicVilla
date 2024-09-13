81d1e18fe87a3223341bd2e9062704a50b47b648
Quan Nguyen <quan.nguyen.se@gmail.com>
2024-09-13 14:46:10 +0700

    First End point

---
+++ MagicVilla.API/Controllers/VillaAPIController.cs
@@ -0,0 +1,23 @@
+﻿using MagicVilla.API.Data;
+using MagicVilla.API.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla.API.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/villaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        }
+    }
+}
